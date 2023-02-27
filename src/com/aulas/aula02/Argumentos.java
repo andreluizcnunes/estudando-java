3a61558564e81742145283928c7853a01a4f5914
André Luiz <andre.doctors@gmail.com>
2023-02-27 08:27:28 -0300

    aula Argumentos pelo terminal

---
+++ src/com/aulas/aula02/Argumentos.java
@@ -0,0 +1,8 @@
+package com.aulas.aula02;
+
+// Quando compilado pelo terminal você precisa passar algo para ser impresso.
+public class Argumentos {
+    public static void main(String[] args) {
+        System.out.println("Você digitou " + args[0]);
+    }
+}
