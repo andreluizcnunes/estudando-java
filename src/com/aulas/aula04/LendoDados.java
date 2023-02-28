9a82508bde72a22246a6e217e2b22b54c7707a00
André Luiz <andre.doctors@gmail.com>
2023-02-28 08:42:36 -0300

    Lendo dados do teclado

---
+++ src/com/aulas/aula04/LendoDados.java
@@ -0,0 +1,29 @@
+package com.aulas.aula04;
+
+import java.util.Scanner;
+
+public class LendoDados {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        String nome = scan.nextLine(); // linha inteira
+//        int idade = scan.nextInt();
+//        double valor = scan.nextDouble();
+
+//        System.out.println("Digite seu nome completo:");
+//        String nomeCompleto = scan.nextLine();
+//        System.out.println("Olá "+nomeCompleto);
+//
+//        System.out.println("Digite seu primeiro nome:");
+//        String nome = scan.next();
+//        System.out.println("Olá "+ nome);
+
+//        System.out.println("Digite sua idade:");
+//        int idade = scan.nextInt();
+//        System.out.println("sua idade é "+idade);
+
+        System.out.println("Valor do Produto: ");
+        double valorProduto = scan.nextDouble();
+        System.out.println("sua idade é "+valorProduto);
+    }
+}
